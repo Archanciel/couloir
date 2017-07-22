72be64446f39d16fe55b70aae5e86a3ad56fd797
Archanciel <jp.schnyder@gmail.com>
2017-07-22 18:36:54 +0200

    new ver improved on phone

--- couloir.py
+++ couloir.py
@@ -1,12 +1,13 @@
 from time import sleep
 import random as r
 
-MAX_WIDTH = 40 #largeur max du couloir
-MIN_WIDTH = 30 #largeur min du couloir
-STEP_LENGTH = 0.0125 #durée pause entre dessin d'une ligne en seconde
-ITERATION_NUMBER = 1000
-STEP_LENGTH = 0.05 #durée pause entre dessin d'une ligne en seconde
-ITERATION_NUMBER = 300
+MAX_SCREEN_WIDTH = 40
+MAX_WIDTH = 11 #largeur max du couloir
+MIN_WIDTH = 2 #largeur min du couloir
+STEP_LENGTH = 0.025 #durée pause entre dessin d'une ligne en seconde
+ITERATION_NUMBER = 500
+#STEP_LENGTH = 0.05 #durée pause entre dessin d'une ligne en seconde
+#ITERATION_NUMBER = 300
 
 
 def populateLine(currWidth, ballX):
@@ -18,14 +19,24 @@ def populateLine(currWidth, ballX):
     '''
     str, leftWallX = fillLeftSpace(currWidth)
     str += '|'
+    isBallDrawn = False
+    ballX -= leftWallX
     
     for i in range(currWidth):
         if i == ballX:
             str += '*'
+            isBallDrawn = True
         else:
             str += ' '
         
-    str += '|'
+    if not isBallDrawn:
+        if ballX < currWidth / 2:
+            str = str.replace("| ", "|*")
+            str += '|'
+        else:
+            str += "*|"
+    else:
+        str += '|'
     
     return str, leftWallX + 1, leftWallX + currWidth
 
@@ -37,7 +48,9 @@ def fillLeftSpace(currWidth):
     dessiner.
     '''
     str = ''
-    spaceNumber = int((MAX_WIDTH - currWidth) / 2)
+    spaceNumber = int((MAX_SCREEN_WIDTH - currWidth) / 2)
+    glitch = r.randint(0, 1)
+    spaceNumber += glitch
     
     for i in range(spaceNumber):
         str += ' '
@@ -50,18 +63,21 @@ def varyCurrWidth(currWidth, variationAmount, variationChangeNumber):
     du couloir.
     '''
     if variationChangeNumber <= 0:
-        variationAmount = r.randint(-1, 1)
-    
+        variationAmount = r.randint(0, 1)
+        if variationAmount == 0:
+            variationAmount = -1
+            
+ #   variationAmount += variationAmount
     currWidth += variationAmount
     
     if currWidth > MAX_WIDTH:
         currWidth = MAX_WIDTH
-        variationAmount = -1 * variationAmount
+        variationAmount = -1
         variationChangeNumber = MAX_VAR_CHANGE_NUMBER
     
     if currWidth < MIN_WIDTH:
         currWidth = MIN_WIDTH
-        variationAmount = -1 * variationAmount
+        variationAmount = 1
         variationChangeNumber = MAX_VAR_CHANGE_NUMBER
 
     return currWidth, variationAmount
@@ -86,22 +102,21 @@ def calculateBallPosition(ballX, ballDirection, leftWallX, rightWallX):
     return ballX, ballDirection
     
 variationAmount = -1 
-currWidth = MAX_WIDTH
+newCurrWidth = oldCurrWidth = MAX_WIDTH
 MAX_VAR_CHANGE_NUMBER = 10
 variationChangeNumber = MAX_VAR_CHANGE_NUMBER
-ballX = int(currWidth / 2) #coordonnée x de la balle
+ballX = int(MAX_SCREEN_WIDTH / 2) #coordonnée x de la balle
 ballDirection = 1
 leftWallX = 1
-rightWallX = currWidth - 1
+rightWallX = newCurrWidth - 1
 
 #boucle principale
 for i in range(ITERATION_NUMBER):
-    ballX, ballDirection = calculateBallPosition(ballX, ballDirection, leftWallX, rightWallX)
-    line, leftWallX, rightWallX = populateLine(currWidth, ballX)
+    line, leftWallX, rightWallX = populateLine(newCurrWidth, ballX)
     print(line)
-    currWidth, variationAmount = varyCurrWidth(currWidth, variationAmount, variationChangeNumber)
+    newCurrWidth, variationAmount = varyCurrWidth(oldCurrWidth, variationAmount, variationChangeNumber)
+    ballX, ballDirection = calculateBallPosition(ballX, ballDirection, leftWallX, rightWallX)
     variationChangeNumber -= 1
- #   ballX, ballDirection = calculateBallPosition(ballX, ballDirection, leftWallX, rightWallX)
     
     if variationChangeNumber <= -1 * MAX_VAR_CHANGE_NUMBER:
         variationChangeNumber = MAX_VAR_CHANGE_NUMBER
